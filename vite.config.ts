50eb3c5534f6a53f02f115c1260b29eaccf762e8
AhmedTrooper <151874789+AhmedTrooper@users.noreply.github.com>
2026-08-04 00:09:04 +0600

    chore(config): explicitly exclude original focus directory from vite optimizations to fix dependency resolution errors

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,11 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [solid()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
   // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
+  optimizeDeps: {
+    entries: ["index.html"],
+  },
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
@@ -25,8 +26,8 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      // 3. tell Vite to ignore watching `src-tauri` and `focus` (the original electron app)
+      ignored: ["**/src-tauri/**", "**/focus/**"],
     },
   },
 }));
